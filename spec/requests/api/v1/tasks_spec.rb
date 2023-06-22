818045193420d714d78a9b9a3d78c32c0d692365
kennankole <kennankole@gmail.com>
2023-06-22 22:50:34 +0300

    Making changes to the API documentation config files

--- spec/requests/api/v1/tasks_spec.rb
+++ spec/requests/api/v1/tasks_spec.rb
@@ -1,12 +1,9 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/v1/tasks', type: :request do
-
   path '/api/v1/tasks' do
-
     get('list tasks') do
       response(200, 'successful') do
-
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
